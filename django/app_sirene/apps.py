b87db35f3dd983a88f860ede3df7f7e6693f9ef1
contact@cavaliba.com <cavaliba>
2024-12-04 15:22:26 +0100

    README

--- django/app_sirene/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppSireneConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_sirene'
-    verbose_name = "APP Sirene"
